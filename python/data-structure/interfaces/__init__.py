712461061efed61ebf59a8bafa919f33594f88c4
elaine <195331169+elainehello@users.noreply.github.com>
2025-11-22 23:27:37 +0100

    upd

---
+++ python/data-structure/interfaces/__init__.py
@@ -0,0 +1,19 @@
+from .protocols import (
+    DataStructure,
+    ListOperation,
+    Stackable,
+    Searchable,
+    Indexable,
+    Iterable
+)
+
+from .base import AbstractList
+__all__ = [
+    "DataStructure",
+    "ListOperation",
+    "Stackable",
+    "Searchable",
+    "Indexable",
+    "Iterable",
+    "AbstractList"
+]
