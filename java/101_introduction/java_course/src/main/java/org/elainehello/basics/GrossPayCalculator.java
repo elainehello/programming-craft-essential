275f866b07ede33ee968f58db8c74ef0c7c0062e
elaine <195331169+elainehello@users.noreply.github.com>
2025-11-23 22:56:10 +0100

    upd

---
+++ java/101_introduction/java_course/src/main/java/org/elainehello/basics/GrossPayCalculator.java
@@ -0,0 +1,18 @@
+package org.elainehello.basics;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String[] args) {
+        System.out.println("How many hours did you work?");
+        Scanner sc = new Scanner(System.in);
+        int hours = sc.nextInt();
+
+        System.out.println("What is your hourly pay rate?");
+        double rate = sc.nextDouble();
+        sc.close();
+
+        double payRate = hours * rate;
+        System.out.println("Your pay rate is: " + payRate);
+    }
+}
