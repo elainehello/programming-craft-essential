63a4310bb7bfa2da2b4b48446597f21fea0f9838
elaine <195331169+elainehello@users.noreply.github.com>
2025-11-24 17:02:57 +0100

    method signatures --prototypes
    Spring Data JPA repository interface

---
+++ java/101_introduction/IdeaProjects/ManagementSystem/src/main/java/com/elainehello/ManagementSystem/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.elainehello.ManagementSystem.repository;
+
+import com.elainehello.ManagementSystem.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.*;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Find user by email (returns 'Optional' to avoid NullPointerException)
+    Optional<User> findByEmail(String email);
+
+    // Find --all users with a given first name (returns List<User>)
+    List<User> findByFirstName(String firstName);
+
+    // Find --all users with a given last name (returns List<User>)
+    List<User> findByLastName(String lastName);
+
+    // Find users by full name (returns List<User>)
+    List<User> findByFirstNameAndLastName(String firstName, String lastName);
+
+    // Delete a user by email (void type)
+    void deleteByEmail(String email);
+
+}
