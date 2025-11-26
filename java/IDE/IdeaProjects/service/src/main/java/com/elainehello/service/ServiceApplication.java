d715fe9362e6a1caad2e5f9df6338e8ca6a336bc
elaine <195331169+elainehello@users.noreply.github.com>
2025-11-26 22:03:09 +0100

    upd

--- java/IDE/IdeaProjects/service/src/main/java/com/elainehello/service/ServiceApplication.java
+++ java/IDE/IdeaProjects/service/src/main/java/com/elainehello/service/ServiceApplication.java
@@ -2,6 +2,13 @@ package com.elainehello.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @SpringBootApplication
 public class ServiceApplication {
@@ -9,5 +16,19 @@ public class ServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
+
+    @Bean
+    RouterFunction<ServerResponse> myRoutes(CustomerRepository customerRepository) {
+        return route()
+                .GET("/customers", req -> ok().body(customerRepository.findAll()))
+                .build();
+    }
+}
+
+record Customer(int id, String name) {
+
+}
+
+interface CustomerRepository extends ListCrudRepository<Customer, Integer> {
 
 }
