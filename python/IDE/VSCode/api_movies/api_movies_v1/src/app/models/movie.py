b7b4332e3501a9752e5c4fcbb8e008c68a2646b7
elaine <195331169+elainehello@users.noreply.github.com>
2025-11-28 16:11:40 +0100

    base
    -- Data loading script for movies
    -- Tables are created by SQLAlchemy models, this script just loads data
    
    -- Copy data from CSV file (uncomment if you want to auto-load data)
    -- COPY movies(poster_link, series_title, released_year, certificate, runtime, genre, imdb_rating, overview, meta_score, director, star1, star2, star3, star4, no_of_votes, gross)
    -- FROM '/docker-entrypoint-initdb.d/imdb_top_1000.csv'
    -- DELIMITER ','
    -- CSV HEADER;

--- python/IDE/VSCode/api_movies/api_movies_v1/src/app/models/movie.py
+++ python/IDE/VSCode/api_movies/api_movies_v1/src/app/models/movie.py
@@ -1,11 +1,9 @@
 from sqlalchemy import Column, Integer, String, Text, Numeric, BigInteger
-from sqlalchemy.ext.declarative import declarative_base
+from ..db.database import Base
 
-BaseModel = declarative_base()
-
-class Movie(BaseModel):
+class Movie(Base):
     __tablename__ = "movies"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     poster_link = Column(Text, nullable=True)
     series_title = Column(String, nullable=False, index=True)
