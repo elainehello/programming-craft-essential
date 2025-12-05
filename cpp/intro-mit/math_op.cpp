4616e975a212c1395ade942fb3bfdd1a632e0a6d
elaine <195331169+elainehello@users.noreply.github.com>
2025-12-05 15:32:56 +0100

    upd

---
+++ cpp/intro-mit/math_op.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+
+    cout << "Please enter a number:\n";
+    cin >> x;
+
+    cout << x / 3 << ' ' << x * 2;
+    return (0);
+}
