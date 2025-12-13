63b644eee9bce50c8b3f75deb64a9957d9e5be78
elaine <195331169+elainehello@users.noreply.github.com>
2025-12-13 18:08:52 +0100

    typo

--- python/python_workshop/07_class_kmsq_country.py
+++ python/python_workshop/07_class_kmsq_country.py
@@ -6,13 +6,16 @@ class Country:
         self.population = population
         self.size_kmsq = size_kmsq
 
-    def size_miles_sq(self, conversio_rate: float=0.621371) -> float:
+    def size_miles_sq(self, conversion_rate: float=0.621371) -> float:
         # miles_sq = size_kmsq * 0.621371^2
-        return self.size_kmsq * conversio_rate ** 2
+        return self.size_kmsq * conversion_rate ** 2
 
 def main() -> None:
     algeria = Country("Algeria", size_kmsq=2.382e6)
     print(algeria.size_miles_sq())
+    venecia = Country("Venecia", size_kmsq=1.98e7)
+    algeria.size_miles_sq(conversion_rate=0.1)
+    print(venecia.size_miles_sq())
 
 if __name__ == "__main__":
     main()
