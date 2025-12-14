2d72882c975c45649ac94df59c676d3f4d123e22
elaine <195331169+elainehello@users.noreply.github.com>
2025-12-14 21:33:02 +0100

    upd

--- python/python_workshop/method/10_static_method.py
+++ python/python_workshop/method/10_static_method.py
@@ -6,15 +6,20 @@ class Diary:
         self.birthday = birthday
         self.christmas = christmas
 
+    @staticmethod
+    def format_date(date: datetime.date) -> str:
+        return date.strftime('%d-%b-%y')
+
     def show_birthday(self) -> str:
-        return self.birthday.strftime('%d-%b-%y')
+        return self.format_date(self.birthday)
 
     def show_christmas(self) -> str:
-        return self.christmas.strftime('%d-%b-%y')
+        return self.format_date(self.christmas)
 
 def main() -> None:
     my_diary = Diary(datetime.date(2022, 7, 30), datetime.date(2023, 7, 30))
     print(my_diary.show_birthday())
+    print(my_diary.show_christmas())
 
 if __name__ == "__main__":
     main()
