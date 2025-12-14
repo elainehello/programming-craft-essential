239b5e432015243944f12707af22aa0765d38d37
elaine <195331169+elainehello@users.noreply.github.com>
2025-12-14 22:12:58 +0100

    type int not str

--- python/python_workshop/method/11_class_method2.py
+++ python/python_workshop/method/11_class_method2.py
@@ -5,7 +5,7 @@ class Country:
     is_human = True
     def __init__(self, name: str,
                 size_kmsq: float,
-                population: str | None = None):
+                population: float | None = None):
         self.name = name
         self.size_kmsq = size_kmsq
         self.population = population
@@ -13,13 +13,13 @@ class Country:
     # notice the use of 'cls' as first argument
     @classmethod
     def create_with_msq(cls: Type["Country"],
-                        name: str, population: str,
+                        name: str, population: float,
                         size_msq: float) -> "Country":
         size_kmsq = size_msq / 0.621371 ** 2
         return cls(name, size_kmsq, population)
 
 def main() -> None:
-    mexico = Country.create_with_msq('Mexico', '150e2', 760000)
+    mexico = Country.create_with_msq('Mexico', 150e6, 760000)
     print(f"Mexico size: {mexico.size_kmsq} km²")
     print(f"Country name: {mexico.name}")
     print(f"Population: {mexico.population}")
