b91a0f4739fa8759360eb9eac648ae1aa3696f27
elaine <195331169+elainehello@users.noreply.github.com>
2025-12-15 11:07:41 +0100

    typo

--- python/python_workshop/properties/04_class_temperature_setter.py
+++ python/python_workshop/properties/04_class_temperature_setter.py
@@ -1,6 +1,6 @@
 class Temperature:
-    def __init__(self, celsius: int) -> None:
-        self.celsius = celsius
+    def __init__(self, celcius: int) -> None:
+        self.celsius = celcius
 
     @property
     def fahrenheit(self) -> float:
@@ -10,13 +10,13 @@ class Temperature:
     def fahrenheit(self, value: int) -> None:
         if value < -460:
             raise ValueError("Temperatures less than -460F are not possible")
-        self.celsius = (value - 32) * 5 / 9
+        self.celcius = (value - 32) * 5 / 9
 
 def main() -> None:
     temp = Temperature(5)
     print(temp.fahrenheit)
     temp.fahrenheit = 32
-    print(temp.celsius)
+    print(temp.celcius)
 
 if __name__ == "__main__":
     main()
