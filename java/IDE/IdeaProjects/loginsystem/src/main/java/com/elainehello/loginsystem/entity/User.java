1b9550918c0e68fb0cf3c124dcd0db48b942ff0f
elaine <195331169+elainehello@users.noreply.github.com>
2025-12-16 17:49:32 +0100

    upd

---
+++ java/IDE/IdeaProjects/loginsystem/src/main/java/com/elainehello/loginsystem/entity/User.java
@@ -0,0 +1,37 @@
+package com.elainehello.loginsystem.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private boolean active = true;
+}
