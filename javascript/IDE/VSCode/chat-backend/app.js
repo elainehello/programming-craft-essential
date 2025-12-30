3e0f6e0b7daaae9fee89febc8299f3cf4b9de9a3
elaine <195331169+elainehello@users.noreply.github.com>
2025-12-31 00:40:12 +0100

    upd

--- javascript/IDE/VSCode/chat-backend/app.js
+++ javascript/IDE/VSCode/chat-backend/app.js
@@ -2,11 +2,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var http = require('http');
+var socketIo = this.request('socket.io');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var chatRouter = requere('./src/routes/chat');
+
+var SocketHandler = require('./src/socket/socketHandler');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// Initialize socket handler
+const socketHandler = new SocketHandler(io);
+app.set('socketHandler', socketHandler);
+
+// Handle socket connections
+io.on('connection', (socket) => {
+    socketHandler.handleConnection(socket);
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +37,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/chat', chatRouter);
+
+// Make server available to bin/www
+app.set('server', server);
 
 module.exports = app;
